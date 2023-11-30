96312cc3485a34d4b62353f988dbcbe2f3d93a95
Karen <65780853+kbei5234@users.noreply.github.com>
2023-11-30 18:14:05 -0500

    Update pattern_gen.vhd
    green block moving down screen and pink block moving across screen

--- pattern_gen.vhd
+++ pattern_gen.vhd
@@ -18,6 +18,19 @@ architecture synth of pattern_gen is
 signal counter : unsigned(30 downto 0) := 31d"0";
 -- signal count : unsigned(2 downto 0) := 3d"0";
 
+-- TODO : create signals for block x and y locations
+-- block x tracking
+signal x_top : unsigned(9 downto 0) := 10d"200";
+signal x_bottom : unsigned(9 downto 0) := 10d"219";
+signal x_left : unsigned(9 downto 0) := 10d"100";
+signal x_right : unsigned(9 downto 0) := 10d"119";
+
+-- block y tracking
+signal y_top : unsigned(9 downto 0) := 10d"400";
+signal y_bottom : unsigned(9 downto 0) := 10d"419";
+signal y_left : unsigned(9 downto 0) := 10d"150";
+signal y_right : unsigned(9 downto 0) := 10d"169";
+
 begin 
 	process(clk60) begin
 		if rising_edge(clk60) then 
@@ -34,16 +47,18 @@ begin
 			
 		--end if;
 	--end process;
+	
+	-- horizontal motion of a block across the screen
 	process(all) begin 
+		-- checking if within valid viewable region
 		if (valid = '0') then 
 			rgb <= "000000";
-		elsif ((row >= 200) and (row <= 229) and (col >= (100 + counter) mod 799) and (col <= (119 + counter) mod 799)) then 
-			-- col <= col mod 799;
-			-- thinking that if we add to the col, then it'll show horizontal movement
-			-- the block is MIA and might be lost in flip flop pass 
-			
-			
+		else 
+			rgb <= "010101";
+		end if;
 		
+		
+		if ((row >= 200) and (row <= 229) and (col >= (100 + counter) mod 799) and (col <= (119 + counter) mod 799)) then 	
 			-- generate alien pattern hopefully please and thank you vhdl 
 			--rgb(1 downto 0) <= (row(9 downto 8) xor col(9 downto 8)) mod 9; 
 			--rgb(3 downto 2) <= (row(7 downto 6) xor col(7 downto 6)) mod 9; 
@@ -51,9 +66,20 @@ begin
 			
 			-- generate a block
 			rgb <= "110011";
-		else
-			-- set background
-			rgb <= "010101";			
+			
+			-- TODO : when pink block leaves, the screen flashes a dark green
+			
+		--else 
+			--rgb <= "010101";
 		end if;
+		
+		-- vertical motion of a block
+		if ((row >= (400 + counter) mod 524) and (row <= (429 + counter) mod 524) and (col >= 150) and (col <= 169)) then
+			-- generate a block
+			rgb <= "001100";
+		--else 
+			--rgb <= "010101";
+		end if;	
 	end process;
+
 end;
