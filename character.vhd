52acb5018bb87dd28117afdccc7691db6dbaab51
roanye <Roan.Yeh@tufts.edu>
2023-12-02 17:18:20 -0500

    Update character.vhd
    fixed some syntax

--- character.vhd
+++ character.vhd
@@ -14,16 +14,16 @@ end character;
 
 architecture synth of character is
         -- Controller_in button mapping
-    -- 0 => right
-    -- 1 => left
-    -- 2 => down
-    -- 3 => up
-    -- 4 => start
-    -- 5 => select
-    -- 6 => B
-    -- 7 => A
-    signal right : std_logic;
-    signal left : std_logic;
+    -- 0 <= rright
+    -- 1 <= lleft
+    -- 2 <= down
+    -- 3 <= up
+    -- 4 <= start
+    -- 5 <= select
+    -- 6 <= B
+    -- 7 <= A
+    signal rright : std_logic;
+    signal lleft : std_logic;
     signal down : std_logic;
     signal up : std_logic;
     signal start : std_logic;
@@ -31,30 +31,30 @@ architecture synth of character is
     signal B : std_logic;
     signal A : std_logic;
 begin;
-    right => controller_ins(0);
-    left => controller_ins(1);
-    down => controller_ins(2);
-    up => controller_ins(3);
-    start => controller_ins(4);
-    sselect => controller_ins(5);
-    B => controller_ins(6);
-    A => controller_ins(7);
+    rright <= controller_ins(0);
+    lleft <= controller_ins(1);
+    down <= controller_ins(2);
+    up <= controller_ins(3);
+    start <= controller_ins(4);
+    sselect <= controller_ins(5);
+    B <= controller_ins(6);
+    A <= controller_ins(7);
     process(all) begin
-        if (right = '1') then
-            positionXout => positionXin + 10;
+        if (rright = '1') then
+            positionXout <= positionXin + 10;
         end if;
-        if (left = '1') then
-            positionXout => positionXin - 10;
+        if (lleft = '1') then
+            positionXout <= positionXin - 10;
         end if;
         if (down = '1') then
-            positionYout => positionYin - 10;
+            positionYout <= positionYin - 10;
         end if;
         if (up = '1') then
-            positionYout => positionYin + 10;
+            positionYout <= positionYin + 10;
         end if;
         if (start = '1') then
-            positionXout => 320;
-            positionYout => 10;
+            positionXout <= 320;
+            positionYout <= 10;
         end if;
     end process;
 end;
